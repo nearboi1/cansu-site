0ca0507714184b767b47bdc891cf27820ff8cfeb
nearboi1 <abdullahbyrm09@gmail.com>
2026-02-23 23:31:04 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+console.log(document.getElementById("jumpBtn"));
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 const jumpBtn = document.getElementById("jumpBtn");
